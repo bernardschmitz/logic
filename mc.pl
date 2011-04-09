74f780df3e843419f7060ce9c22c3eab16c3d2e8
Bernard Schmitz <bernard@maha.homeip.net>
2011-04-09 15:55:13 -0700

    microcode

--- mc.pl
+++ mc.pl
@@ -99,9 +99,78 @@ my %microcode = (
 		]
 	],
 
-	j => [
-		{ fetch => 0, op => $ins_j, t => $op_t }, [ 
-			{ pc_src => $pc_src_op, pc_wrt => $pc_wrt_1, t_reset => 1, fetch => 1 } 
+	sub => [
+		{ fetch => 0, op => $ins_sub, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_sub, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	mul => [
+		{ fetch => 0, op => $ins_mul, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_mul, lo_wrt => 1, hi_wrt => 1, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	div => [
+		{ fetch => 0, op => $ins_div, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_div, lo_wrt => 1, hi_wrt => 1, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	sll => [
+		{ fetch => 0, op => $ins_sll, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_op, alu_op => $alu_op_sll, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	srl => [
+		{ fetch => 0, op => $ins_srl, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_op, alu_op => $alu_op_srl, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	sra => [
+		{ fetch => 0, op => $ins_sra, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_op, alu_op => $alu_op_sra, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	sllv => [
+		{ fetch => 0, op => $ins_sllv, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, reg_src2_src => $reg_src2_src_rt, alu_op => $alu_op_sll, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	srlv => [
+		{ fetch => 0, op => $ins_srlv, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, reg_src2_src => $reg_src2_src_rt, alu_op => $alu_op_srl, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	srav => [
+		{ fetch => 0, op => $ins_srav, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, reg_src2_src => $reg_src2_src_rt, alu_op => $alu_op_sra, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	beq => [
+		{ fetch => 0, op => $ins_beq, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, reg_src2_src => $reg_src2_src_rd, alu_op => $alu_op_add, result_wrt => 0 }, 
+			{ pc_src => $pc_src_pc_op, pc_wrt => $pc_wrt_alu_eq, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	bne => [
+		{ fetch => 0, op => $ins_bne, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, reg_src2_src => $reg_src2_src_rd, alu_op => $alu_op_add, result_wrt => 0 }, 
+			{ pc_src => $pc_src_pc_op, pc_wrt => $pc_wrt_not_alu_eq, t_reset => 1, fetch => 1 } 
 		]
 	],
 
@@ -112,10 +181,79 @@ my %microcode = (
 		]
 	],
 
-	beq => [
-		{ fetch => 0, op => $ins_beq, t => $op_t }, [ 
-			{ alu_b_src => $alu_b_src_reg2, reg_src2_src => $reg_src2_src_rd, alu_op => $alu_op_add, result_wrt => 0 }, 
-			{ pc_src => $pc_src_pc_op, pc_wrt => $pc_wrt_alu_eq, t_reset => 1, fetch => 1 } 
+	slti => [
+		{ fetch => 0, op => $ins_slti, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_op, alu_op => $alu_op_add, result_wrt => 0 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_alu_lt, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	and => [
+		{ fetch => 0, op => $ins_and, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_and, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	andi => [
+		{ fetch => 0, op => $ins_andi, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_op, alu_op => $alu_op_and, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	or => [
+		{ fetch => 0, op => $ins_or, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_or, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	ori => [
+		{ fetch => 0, op => $ins_ori, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_op, alu_op => $alu_op_or, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	xor => [
+		{ fetch => 0, op => $ins_xor, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_xor, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	nor => [
+		{ fetch => 0, op => $ins_nor, t => $op_t }, [ 
+			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_nor, result_wrt => 1 }, 
+			{ reg_wrt => 1, reg_src => $reg_src_result, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	j => [
+		{ fetch => 0, op => $ins_j, t => $op_t }, [ 
+			{ pc_src => $pc_src_op, pc_wrt => $pc_wrt_1, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	jr => [
+		{ fetch => 0, op => $ins_jr, t => $op_t }, [ 
+			{ pc_src => $pc_src_reg2, pc_wrt => $pc_wrt_1, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+	jal => [
+		{ fetch => 0, op => $ins_jal, t => $op_t }, [ 
+			{ reg_src => $reg_src_pc, reg_wrt => 1 }, 
+			{ pc_src => $pc_src_op, pc_wrt => $pc_wrt_1, t_reset => 1, fetch => 1 } 
+		]
+	],
+
+
+	jalr => [
+		{ fetch => 0, op => $ins_jalr, t => $op_t }, [ 
+			{ reg_src => $reg_src_pc, reg_wrt => 1 }, 
+			{ pc_src => $pc_src_reg2, pc_wrt => $pc_wrt_1, t_reset => 1, fetch => 1 } 
 		]
 	],
 
@@ -133,30 +271,25 @@ my %microcode = (
 		]
 	],
 
+	mfhi => [
+		{ fetch => 0, op => $ins_mfhi, t => $op_t }, [
+			{ reg_wrt => 1, reg_src => $reg_src_hi, t_reset => 1, fetch => 1 }
+		]
+	],
+
+	mflo => [
+		{ fetch => 0, op => $ins_mflo, t => $op_t }, [
+			{ reg_wrt => 1, reg_src => $reg_src_lo, t_reset => 1, fetch => 1 }
+		]
+	],
+
 	halt => [
 		{ fetch => 0, op => $ins_halt, t => $op_t }, [ 
 			{ halt => 1 }
 		]
 	],
 
-	jalr => [
-		{ fetch => 0, op => $ins_jalr, t => $op_t }, [ 
-			{ reg_src => $reg_src_pc, reg_wrt => 1 }, 
-			{ pc_src => $pc_src_reg2, pc_wrt => $pc_wrt_1, t_reset => 1, fetch => 1 } 
-		]
-	],
 
-	mul => [
-		{ fetch => 0, op => $ins_mul, t => $op_t }, [ 
-			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_mul, lo_wrt => 1, hi_wrt => 1, t_reset => 1, fetch => 1 } 
-		]
-	],
-
-	div => [
-		{ fetch => 0, op => $ins_div, t => $op_t }, [ 
-			{ alu_b_src => $alu_b_src_reg2, alu_op => $alu_op_div, lo_wrt => 1, hi_wrt => 1, t_reset => 1, fetch => 1 } 
-		]
-	],
 
 
 );
