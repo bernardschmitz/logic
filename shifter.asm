f01ac9e83d32bed1fff494129a78b0fd8ff32182
Bernard Schmitz <bernard@maha.homeip.net>
2011-04-10 20:39:24 -0700

    more tests

--- shifter.asm
+++ shifter.asm
@@ -3,13 +3,23 @@
 	addi	r1, r0, 1
 	addi	r2, r0, 10
 
-loop:
+loop0:
 	sll	r1, r1, 1
 	sw	r1, r3, 0
 	addi	r3, r3, 1
 	
 	addi	r2, r2, -1
-	bne	r2, r0, loop
+	bne	r2, r0, loop0
+
+	addi	r1, r0, 8000
+	addi	r2, r0, 10
+loop1:
+	srl	r1, r1, 1
+	sw	r1, r3, 0
+	addi	r3, r3, -1
+	
+	addi	r2, r2, -1
+	bne	r2, r0, loop1
 
 	halt
 
