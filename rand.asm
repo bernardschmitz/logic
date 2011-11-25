8f38338c631a1a3a137c9a37f3ce098cb5875d61
Bernard Schmitz <bernard@maha.homeip.net>
2011-11-24 17:21:56 -0800

    changed rand

--- rand.asm
+++ rand.asm
@@ -1,16 +1,9 @@
 
 loop:
-	lw	t1, zero, 0f002	; load random
-	andi	t2, t0, 1f	; get screen row
-	sw	t1, t2, 0a000	; send random num to screen row
-
 	lw	t1, zero, 0f002 ; get random num
 	andi	t2, t1, 1f
 	addi	t2, t2, 20
 	sw	t2, zero, 0fffe ; write random character
 
-	inc	t0
-
 	j	loop
-
 
