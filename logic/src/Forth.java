b7b1b662f4d68c5dd221dc2c5a3d62a50514645e
Bernard Schmitz <bernard@maha.homeip.net>
2013-08-11 16:15:21 -0700

    better

--- logic/src/Forth.java
+++ logic/src/Forth.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Forth {
 
 	private static final int MEMORY_SIZE = 2000;
@@ -5,37 +8,15 @@ public class Forth {
 
 	private final int memory[] = new int[MEMORY_SIZE];
 
+	private final List<String> strings = new ArrayList<String>();
+
 	private final int returnStackAddr = 0;
 	private final int dataStackAddr = 1;
 	private final int instructionAddr = 2;
 	private final int wordAddr = 3;
 	private final int execAddr = 4;
 
-	private final Primitive primitives[] = new Primitive[20];
-
-	{
-		this.primitives[0] = new Lit();
-		this.primitives[1] = new Emit();
-		this.primitives[2] = new Halt();
-		this.primitives[3] = new Plus();
-	}
-
-	{
-		this.memory[10] = 0;
-		this.memory[11] = 1;
-		this.memory[12] = 2;
-		this.memory[13] = 3;
-
-		this.memory[100] = 10;
-		this.memory[101] = '*';
-		this.memory[102] = 11;
-		this.memory[103] = 10;
-		this.memory[104] = 1;
-		this.memory[105] = 10;
-		this.memory[106] = 2;
-		this.memory[107] = 13;
-		this.memory[108] = 12;
-	}
+	private final List<Primitive> primitives = new ArrayList<Primitive>();
 
 	public static void main(final String[] args) {
 
@@ -46,11 +27,54 @@ public class Forth {
 		super();
 	}
 
+	private int[] defcode(int prev, int here, String name, int flags,
+			Primitive code) {
+
+		int back = here;
+		memory[here++] = prev;
+		memory[here++] = flags;
+		strings.add(name);
+		memory[here++] = strings.size() - 1;
+		// memory[here++] = back;
+
+		int cfa = here;
+		// memory[here] = here + 1;
+		// here++;
+		primitives.add(code);
+		memory[here++] = primitives.size() - 1;
+
+		return new int[] { back, cfa, here };
+	}
+
 	private void init() {
 
 		this.memory[this.returnStackAddr] = MEMORY_SIZE - 1;
 		this.memory[this.dataStackAddr] = MEMORY_SIZE - RETURN_SIZE;
 		this.memory[this.instructionAddr] = 100;
+
+		int[] code = defcode(0, 10, "lit", 0, new Lit());
+		int LIT = code[1];
+		code = defcode(code[0], code[2], "emit", 0, new Emit());
+		int EMIT = code[1];
+		code = defcode(code[0], code[2], "+", 0, new Plus());
+		int PLUS = code[1];
+		code = defcode(code[0], code[2], "halt", 0, new Halt());
+		int HALT = code[1];
+
+		this.memory[100] = LIT;
+		this.memory[101] = '*';
+		this.memory[102] = EMIT;
+		this.memory[103] = LIT;
+		this.memory[104] = 1;
+		this.memory[105] = LIT;
+		this.memory[106] = 2;
+		this.memory[107] = PLUS;
+		this.memory[108] = HALT;
+
+		loop();
+	}
+
+	private void loop() {
 
 		while (true) {
 			this.next();
@@ -66,7 +90,7 @@ public class Forth {
 		this.memory[this.wordAddr] = w;
 		final int x = this.memory[w];
 		this.memory[this.execAddr] = x;
-		this.primitives[x].exec();
+		this.primitives.get(x).exec();
 	}
 
 	private void pushRs(final int v) {
@@ -138,7 +162,7 @@ public class Forth {
 			System.err.println("Plus");
 			int a = popDs();
 			int b = popDs();
-			pushDs(a+b);
+			pushDs(a + b);
 		}
 	}
 
